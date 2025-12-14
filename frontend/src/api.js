2f668488ddd5e3a7e571b6e5dee7d01067ada055
vishalnotfound <ping4vishal@gmail.com>
2025-12-14 17:16:27 +0530

    frontend ready

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
-// Add token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) {
